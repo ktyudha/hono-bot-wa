0fa500c18fdfdf962fa9ae8e052c4cebfadcb570
ktyudha <ktyudha@gmail.com>
2025-11-03 19:54:00 +0700

    feat: init bot

--- src/core/whatsapp.core.ts
+++ src/core/whatsapp.core.ts
@@ -3,4 +3,7 @@ import { WhatsAppBotService } from "@/services/whatsappBot.service";
 
 export default async function whatsappInitialize() {
   await whatsappService.initialize();
+
+  new WhatsAppBotService();
+  console.log("[INIT] WhatsApp bot is now active");
 }
