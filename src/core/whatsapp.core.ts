7325f27e169852dd502c226121062abf212d5156
ktyudha <ktyudha@gmail.com>
2025-11-03 19:37:24 +0700

    feat: add bot

--- src/core/whatsapp.core.ts
+++ src/core/whatsapp.core.ts
@@ -1,4 +1,5 @@
 import { whatsappService } from "@/services/whatsapp.service";
+import { WhatsAppBotService } from "@/services/whatsappBot.service";
 
 export default async function whatsappInitialize() {
   await whatsappService.initialize();
